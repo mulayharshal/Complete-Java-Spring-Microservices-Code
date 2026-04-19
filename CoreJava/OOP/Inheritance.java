ee8b61abb65d7d18403a4700d91a9c7ec1f87241
Harshal Mulay <harshalmulay1039@gmail.com>
2026-04-19 12:12:03 +0530

    inheritance

---
+++ CoreJava/OOP/Inheritance.java
@@ -0,0 +1,38 @@
+package CoreJava.OOP;
+
+public class Inheritance {
+    public static void main(String [] args){
+        VeryAdvCalc cal=new VeryAdvCalc();
+        int r1=cal.add(3,8);
+        int r2=cal.sub(9,3);
+        int r3=cal.multi(7,4);
+        int r4=cal.div(34,12);
+        double r5=cal.power(3,2);
+
+        System.out.println(r1+" "+r2+" "+r3+" "+r4+" "+r5);
+
+    }
+}
+
+class Calc{
+    int add(int a,int b){
+        return a+b;
+    }
+    int sub(int a,int b){
+        return a-b;
+    }
+}
+class AdvCalc extends Calc{
+    int multi(int a,int b){
+        return a*b;
+    }
+    int div(int a,int b){
+        return a/b;
+    }
+
+}
+class VeryAdvCalc extends AdvCalc{
+    double power(int a,int b){
+        return Math.pow(a, b);
+    }
+}
